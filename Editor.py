c7cb8e64d53f82a23386cc45108b2f948501ef9e
Guillaume <gparis@et.esiea.fr>
2023-11-05 17:54:06 +0100

    Revert "commit"
    This reverts commit fd15f7acf6760aa81cfa8fac6045a8ffdaa3438a.

--- Editor.py
+++ Editor.py
@@ -2,16 +2,13 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtNetwork import *
-import datetime
+import os, stat, sys, datetime, requests, json
+from PIL import Image, ExifTags
 
-from Data import dataProcessing
-from imageP import imageProcessing
 class EditorWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Editeur")
-        self.data = dataProcessing()
-        self.image = imageProcessing()
         self.init_editor()
         self.show()
 
@@ -44,18 +41,103 @@ class EditorWindow(QMainWindow):
             selected_files = file_dialog.selectedFiles()
             if selected_files:
                 file_path = selected_files[0]
-                if self.image.isImg(file_path):
-                    self.file_info_label.setText(self.data.file_info(file_path))
-                    self.text_edit.setText(str(self.image.exifdata(file_path)))
+                if self.isImg(file_path):
+                    self.file_info_label.setText(self.file_info(file_path))
+                    self.text_edit.setText(str(self.exifdata(file_path)))
                 else :
                     self.central_widget.layout().addWidget(self.button_save_file)
-                    self.file_info_label.setText(self.data.file_info(file_path))                                   
-                    self.text_edit.setPlainText(self.data.hexdump(file_path))
+                    self.file_info_label.setText(self.file_info(file_path))                                   
+                    self.text_edit.setPlainText(self.hexdump(file_path))
                     self.current_file_path = file_path
+
+    def isImg(self,file_path):
+        try:
+            Image.open(file_path)
+        except IOError:
+            return False
+        return True
+    
+    def exifdata(self, file_path):
+        try:
+            with Image.open(file_path) as image:
+                exif_info = image._getexif()
+
+                if exif_info:
+                    exif_data = {}
+                    for tag, value in exif_info.items():
+                        tag_name = ExifTags.TAGS.get(tag, tag)
+                        exif_data[tag_name] = value
+
+                    exif_json = json.dumps(exif_data, default=str, indent=4)
+                    with open('export.json', 'w') as export_file:
+                        export_file.write(exif_json)
+
+                    return exif_data
+                else:
+                    return "No Exif data found in the image."
+        except (IOError, AttributeError, KeyError) as e:
+            return f"Error while reading Exif data: {str(e)}"
+
+    def file_info(self, file_path):
+        try:
+            # Get file statistics
+            file_stat = os.stat(file_path)
+
+            # Extract information from file_stat
+            file_name = os.path.basename(file_path)
+            file_size = file_stat.st_size
+            file_permissions = stat.filemode(file_stat.st_mode)
+            access_time = datetime.datetime.fromtimestamp(file_stat.st_atime)
+            modification_time = datetime.datetime.fromtimestamp(file_stat.st_mtime)
+
+            info = f"File Name: {file_name}\n"
+            info += f"File Path: {file_path}\n"
+            info += f"File Size: {file_size} bytes\n"
+            info += f"File Permissions: {file_permissions}\n"
+            info += f"Last Access Time: {access_time}\n"
+            info += f"Last Modification Time: {modification_time}\n"
+            return info
+
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def hexdump(self, file_path, bytes_per_line=16):
+        with open(file_path, 'rb') as file:
+            offset = 0
+            hexdump_text = ""
+            while True:
+                data = file.read(bytes_per_line)
+                if not data:
+                    break
+                hex_data = ' '.join(f'{byte:02X}' for byte in data)
+                ascii_data = ''.join(chr(byte) if 32 <= byte < 127 else '.' for byte in data)
+                hexdump_text += f'{offset:08X}: {hex_data.ljust(3 * bytes_per_line)}  {ascii_data}' + '\n'
+                offset += bytes_per_line
+        return hexdump_text
+
+    def reverse_hexdump(self, hexdump_text):
+        binary_data = bytearray()
+        lines = hexdump_text.split('\n')
+        for line in lines:
+            # Split the line into sections, excluding the offset part
+            parts = line.split(':')
+            if len(parts) > 1:
+                hex_data = parts[1].split()
+                for hex_byte in hex_data:
+                    try:
+                        byte_value = int(hex_byte, 16)
+                        binary_data.append(byte_value)
+                    except ValueError:
+                        # Handle non-hex characters, such as comments
+                        pass
+        return bytes(binary_data)
+
 
     def save_file(self):
         if self.current_file_path:
-            bytes = self.data.reverse_hexdump(self.text_edit.toPlainText())
+            bytes = self.reverse_hexdump(self.text_edit.toPlainText())
             try:
                 with open(self.current_file_path, 'wb') as file:
                     file.write(bytes)
